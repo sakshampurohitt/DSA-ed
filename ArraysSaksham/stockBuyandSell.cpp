8997722b022753f8a793cb56259cede94baffd05
sakshampurohitt <127320787+sakshampurohitt@users.noreply.github.com>
2024-12-25 22:13:25 +0530

    Create stockBuyandSell.cpp

---
+++ ArraysSaksham/stockBuyandSell.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        int min_price = INT_MAX;
+        int max_profit = 0;
+        for (int price : prices) {
+            if (price < min_price) {
+                min_price = price; 
+            } else if (price - min_price > max_profit) {
+                max_profit = price - min_price;  
+            }
+        }
+
+        return max_profit;
+    }
+};
