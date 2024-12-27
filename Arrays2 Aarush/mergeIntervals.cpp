d591e896f207e294527270d78c85c5d62d828892
Aarush Gupta <71131751+aarush-glitch@users.noreply.github.com>
2024-12-27 19:50:10 +0530

    Create mergeIntervals.cpp

---
+++ Arrays2 Aarush/mergeIntervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int k=0;
+
+        for(int i=1; i<intervals.size(); i++) {
+            if(intervals[k][1]>=intervals[i][0]) {
+                intervals[k][1]=max(intervals[k][1], intervals[i][1]);
+            }
+            else {
+                k++;
+                intervals[k]=intervals[i];
+            }
+        }
+
+        intervals.resize(k+1);
+        return intervals;
+    }
+};
