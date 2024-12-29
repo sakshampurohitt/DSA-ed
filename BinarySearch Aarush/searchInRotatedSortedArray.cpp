4e519edf6318ff220c4aa6d90361134fa157cfce
Aarush Gupta <71131751+aarush-glitch@users.noreply.github.com>
2024-12-30 00:41:22 +0530

    Create searchInRotatedSortedArray.cpp

---
+++ BinarySearch Aarush/searchInRotatedSortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0, h=nums.size()-1;
+        while(l<=h) {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid] >= nums[l]) {
+                if(nums[mid]>=target && nums[l]<=target) h=mid-1;
+                else l=mid+1;
+            }
+            else {
+                if(nums[mid]<=target && nums[h]>=target) l=mid+1;
+                else h=mid-1;
+            }
+        }
+        return -1;
+    }
+};
